ee09594c7bce5f10f41fa1ee0f17c2620d499efe
Sangeetha <67860721+SangeethaMuniappan@users.noreply.github.com>
2020-08-03 15:40:31 +0530

    Update ProfileFrag.java

--- app/src/main/java/com/aushadh/hospital/Fragment/ProfileFrag.java
+++ app/src/main/java/com/aushadh/hospital/Fragment/ProfileFrag.java
@@ -1,71 +1,103 @@
-package com.aushadh.hospital.Fragment;
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    tools:context=".Fragment.ProfileFrag">
+    <RelativeLayout
+        android:background="@color/colorPrimaryDark"
+        android:padding="@dimen/size_24"
+        android:layout_width="match_parent"
+        android:layout_height="200sp">
+        <ImageView
+            android:id="@+id/userImage"
+            android:src="@drawable/user"
+            android:layout_centerHorizontal="true"
+            android:layout_width="120sp"
+            android:layout_height="120sp" />
+
+        <TextView
+            android:id="@+id/mobile"
+            android:textColor="@color/white"
+            android:layout_marginTop="@dimen/margin_top"
+            android:textAlignment="center"
+            android:text="@string/mobile"
+            android:layout_below="@id/userImage"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content" />
+    </RelativeLayout>
+    <RelativeLayout
+        android:padding="@dimen/parent_layout_padding"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent">
 
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.aushadh.hospital.App.SessionManager;
-import com.aushadh.hospital.CreateAlarm;
-import com.aushadh.hospital.MainActivity;
-import com.aushadh.hospital.R;
-
-import java.util.Objects;
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ProfileFrag extends Fragment {
-SessionManager sessionManager ;
-View view ;
-TextView userMobile ;
-private Button createRemainder;
-
-    //SessionManager sessionManager = new SessionManager()
-    public ProfileFrag() {
-        // Required empty public constructor
-    }
+        <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="match_parent"
+            android:orientation="vertical">
+            <Button
+                android:id="@+id/create"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:drawableStart="@drawable/ic_add_alarm_black_24dp"
+                android:drawablePadding="@dimen/size_2"
+                android:drawableTint="@color/colorPrimaryDark"
+                android:text="@string/remainder"
+                android:textAlignment="viewStart"
+                android:textColor="@color/colorPrimaryDark"
+                android:theme="@style/FlatButton" />
+            <!--<Button-->
+                <!--android:layout_marginTop="@dimen/size_4"-->
+                <!--android:drawableTint="@color/colorPrimaryDark"-->
+                <!--android:textColor="@color/colorPrimaryDark"-->
+                <!--android:drawablePadding="@dimen/size_2"-->
+                <!--android:drawableStart="@drawable/ic_alarm_black_24dp"-->
+                <!--android:text="@string/remainder_m"-->
+                <!--android:textAlignment="viewStart"-->
+                <!--android:theme="@style/FlatButton"-->
+                <!--android:layout_width="match_parent"-->
+                <!--android:layout_height="wrap_content" />-->
 
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_profile, container, false);
-        userMobile= view.findViewById(R.id.mobile);
-        sessionManager = new SessionManager( Objects.requireNonNull( getContext() ) );
-        userMobile.setText("+91-"+sessionManager.getLoggedInMobile()   );
-        createRemainder = view.findViewById(R.id.create) ;
-        createRemainder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent c     = new Intent(getContext() , CreateAlarm.class);
-                c.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(c);
-            }
-        });
-        Button logout = view.findViewById( R.id.logout );
-        logout.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sessionManager.setLogin(false);
-                sessionManager.setLoginMobile(null);
-                Intent home = new Intent(getContext() , MainActivity.class);
-                home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
-                startActivity(home);
-                Objects.requireNonNull( getActivity() ).finish();
-            }
-        });
-        return view ;
-    }
 
-}
+        </LinearLayout>
+
+        <Button
+            android:id="@+id/Doctor_details"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_alignParentBottom="true"
+            android:layout_marginBottom="417dp"
+            android:background="#ffffff"
+            android:text="    Doctor Lists"
+            android:textAlignment="viewStart"
+            android:textColor="@color/colorPrimaryDark" />
+
+        <Button
+            android:id="@+id/feedback"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_alignParentBottom="true"
+            android:layout_marginBottom="365dp"
+            android:background="#ffffff"
+            android:text="     Feedback"
+            android:textAlignment="viewStart"
+            android:textColor="@color/colorPrimaryDark" />
+
+
+        <Button
+            android:id="@+id/logout"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_alignParentBottom="true"
+            android:layout_marginBottom="@dimen/size_54"
+            android:layout_marginStart="145dp"
+            android:background="@color/colorPrimary"
+            android:text="logout"
+            android:textColor="@color/white" />
+    </RelativeLayout>
+
+
+</LinearLayout>
